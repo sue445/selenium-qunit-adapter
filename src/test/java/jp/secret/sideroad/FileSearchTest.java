0a706d435523e7f0d5c72505a2457dc7a6a33a6b
sue445 <sue445@sue445.net>
2012-04-06 23:40:29 +0900

    add empty test

---
+++ src/test/java/jp/secret/sideroad/FileSearchTest.java
@@ -0,0 +1,15 @@
+package jp.secret.sideroad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileSearchTest {
+	private FileSearch fileSearch = new FileSearch();
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
