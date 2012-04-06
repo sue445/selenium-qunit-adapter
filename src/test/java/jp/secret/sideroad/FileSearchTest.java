1bddc9b0c3eba14acb49f2fa70e0788011f9e198
sue445 <sue445@sue445.net>
2012-04-07 01:18:19 +0900

    add test: listFiles

--- src/test/java/jp/secret/sideroad/FileSearchTest.java
+++ src/test/java/jp/secret/sideroad/FileSearchTest.java
@@ -1,5 +1,10 @@
 package jp.secret.sideroad;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+
 import jp.secret.sideroad.rules.TemporaryFile;
 
 import org.junit.Rule;
@@ -9,12 +14,20 @@ public class FileSearchTest {
 	private FileSearch fileSearch = new FileSearch();
 
 	@Rule
-	public TemporaryFile tempFile = new TemporaryFile("a.txt");
+	public TemporaryFile tempFile = new TemporaryFile("a.txt", "b.java");
 
 
 	@Test
 	public void exists() throws Exception{
 		tempFile.assertExistsFile("a.txt");
+	}
+
+	@Test
+	public void listFiles() throws Exception {
+		File[] actual = fileSearch.listFiles(tempFile.getRootPath(), "*.java");
+
+		assertThat(actual.length, is(1));
+		assertThat(actual[0].getName(), is("b.java"));
 	}
 
 }
